8649f4ff5e40e38d4864d70ff0aadba1ff955ff1
dilipkumarmanavalla <42508793+dilipkumarmanavalla@users.noreply.github.com>
2024-05-10 16:41:18 +0530

    added test cases for polarity_check

---
+++ polarity_check/tests/test_polarity_check.py
@@ -0,0 +1,52 @@
+import pytest
+import httpx
+from fastapi.testclient import TestClient
+from app import main
+import requests
+
+client = TestClient(main.app)
+
+
+def test_health_check():
+    response = client.get("/health_check")
+    assert response.status_code == 200
+    data = response.json()
+    assert data["Status"] == "Up"
+
+
+def test_analyse_and_generate_polarity():
+    comments = [
+        {"id": 1, "username": "user1", "text": "positive comment", "created_at": "20240425100000"},
+        {"id": 2, "username": "user2", "text": "negative comment", "created_at": "20240425T110000"}
+    ]
+    updated_comments = main.analyse_and_generate_polarity(comments)
+    assert len(updated_comments) == 2
+    assert updated_comments[0]["polarity_score"] == 1
+    assert updated_comments[0]["polarity"] == "positive"
+    assert updated_comments[1]["polarity_score"] == 0
+    assert updated_comments[1]["polarity"] == "negative"
+
+
+def test_get_comments():
+    hosts = ['0.0.0.0', 'localhost']
+    for each_host in hosts:
+        FEDDIT_URL = f"http://{each_host}:8080/api/v1"
+        try:
+            response = requests.get(FEDDIT_URL)
+        except:
+            class T:status_code = 500
+            response = T
+        if response.status_code == 200:
+            main.FEDDIT_URL = f"http://{each_host}:8080"
+            response = client.get("/comments", params={"subfeddit_id": "1"})
+            assert response.status_code == 200
+            data = response.json()
+            assert "comments" in data
+            assert len(data["comments"]) == 25
+            continue
+
+    main.FEDDIT_URL = f"http://test_500:8080/api/v1"
+    response = client.get("/comments", params={"subfeddit_id": "1"})
+    print(response.json())
+    assert response.status_code == 500
+    print('server is not available')
