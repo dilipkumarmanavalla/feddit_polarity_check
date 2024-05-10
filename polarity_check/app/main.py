a735a7319b0df0525f74c695394fa1d412aaceef
dilipkumarmanavalla <42508793+dilipkumarmanavalla@users.noreply.github.com>
2024-05-10 17:09:06 +0530

    changes as per the pylint

--- polarity_check/app/main.py
+++ polarity_check/app/main.py
@@ -1,15 +1,19 @@
+"""
+Main module for the Polarity Check API
+"""
+
 from fastapi import FastAPI, HTTPException, Query
-from app.models import Comment
+from models import Comment  # Importing Comment model from models module
 import requests
 from nltk.sentiment import SentimentIntensityAnalyzer
-sia = SentimentIntensityAnalyzer()
 
+sia = SentimentIntensityAnalyzer()
 
 app = FastAPI()
 
 FEDDIT_URL = "http://feddit:8080"
 
-
+# Endpoint to retrieve comments from Feddit
 @app.get("/comments")
 async def get_comments(
     subfeddit_id: str,
@@ -28,6 +32,7 @@ async def get_comments(
         response = requests.get(
             f"{FEDDIT_URL}/api/v1/comments",
             params=params,
+            timeout=10
         )
         response.raise_for_status()
         response = dict(response.json())
@@ -37,12 +42,13 @@ async def get_comments(
     if len(response['comments']) > 0:
         comments = response['comments']
 
+        # Filter comments based on start_time and end_time
         if start_time is not None:
             comments = [comment for comment in comments if comment["created_at"] >= start_time]
         if end_time is not None:
             comments = [comment for comment in comments if comment["created_at"] <= end_time]
 
-        # Sentiment Analysis For Checking Comments on given subfeddit are positive or negative
+        # Perform Sentiment Analysis on comments
         comments = analyse_and_generate_polarity(comments)
 
         if sort_by_score:
@@ -57,23 +63,24 @@ async def get_comments(
 
     return response
 
-
+# Function to perform Sentiment Analysis and generate polarity score for comments
 def analyse_and_generate_polarity(comments):
     updated_comments = []
     for i in comments:
         temp_dict = dict(i)
         sentiment_scores = sia.polarity_scores(temp_dict['text'])
+
         if sentiment_scores['neg'] <= sentiment_scores['pos']:
-            score,polarity=1,'positive'
+            score, polarity = 1, 'positive'
         else:
-            score,polarity=0,'negative'
+            score, polarity = 0, 'negative'
 
         temp_dict['polarity_score'] = score
-        temp_dict['polarity']=polarity
+        temp_dict['polarity'] = polarity
         updated_comments.append(temp_dict)
     return updated_comments
 
-
+# Endpoint for health check
 @app.get("/health_check")
 async def health_check():
     return {"Status": 'Up'}
